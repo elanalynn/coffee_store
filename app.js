b616d93a5701fe19d6e1013c52ff87e264e1d359
Elana Kopelevich <ekopelevich@gmail.com>
2018-04-06 18:30:49 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var c1 = document.getElementById("coffee1");
+  c1.addEventListener("click", function() {
+    c1.className = "special";
+  });
+});
